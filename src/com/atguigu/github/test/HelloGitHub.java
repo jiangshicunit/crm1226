33ce78a33ad84dd07f3ff36b705e8465abe840f9
jiangshicun <jiangshicun@jiangshicun-PC>
2016-05-27 22:27:33 +0800

    update 05 27

--- src/com/atguigu/github/test/HelloGitHub.java
+++ src/com/atguigu/github/test/HelloGitHub.java
@@ -6,6 +6,7 @@ public class HelloGitHub {
 		System.out.println("github 你好！");
 		System.out.println("new chenge");
 		System.out.println("I love you");
+		System.out.println("github 你好！1");
 	}
 
 }
